cc17e77238b7a71739aa6a5a21912ab706586029
ErikCR1 <ecatalanrodriguez.25cf@iesesteveterradas.cat>
2026-01-21 00:42:40 +0100

    cambios

--- M3_Programacio/funcions.py
+++ M3_Programacio/funcions.py
@@ -5,11 +5,23 @@ def conectar():
     return mysql.connector.connect(
         host="127.0.0.1",
         port=3308,
-        user="super",
+        user="admin",
         password="1234",
         database="choose_your_story"
     )
 #--------------------------------------
+
+def buscar_usuario(query):
+    conexion = conectar()
+    cursor = conexion.cursor()
+
+    a, b = str(query).split(";")
+    buscar = "SELECT id_usuari FROM usuari WHERE username = %s and password = %s"
+    cursor.execute(buscar, (a,b))
+    resultado = cursor.fetchone()
+    cursor.close()
+    conexion.close()
+    return resultado
 
 #-----------------Login----------------
 def login():
@@ -24,11 +36,13 @@ def login():
     buscar = "SELECT password FROM usuari WHERE username = %s"
     cursor.execute(buscar, (user,))
     resultado = cursor.fetchone()
-
+    
+    string =  f"{user};{password}"
+    
     if resultado:
         if resultado[0] == password:
             print(f"S'ha iniciat sessió correctament. Benvingut de nou, {user}!")
-            return True
+            return string
         else:
             print("Error: La contrasenya és incorrecta per a aquest usuari.")
             return False
@@ -43,7 +57,7 @@ def login():
             # CIERRA AQUÍ
             cursor.close()
             conexion.close()
-            return True
+            return string
         
         except Exception as e:
             # Y AQUÍ TAMBIÉN POR SI HAY ERROR
@@ -51,14 +65,50 @@ def login():
             conexion.close()
             return False
         
-    
-def personatge_aventura():
+def cargar_aventura():
     conexion = conectar()
+    cursor = conexion.cursor(dictionary=True)
+
+    print ("----AVENTURAS DISPONIBLES----")
+    cursor.execute("SELECT id_aventura, nom, descripcio FROM aventura")
     
+    lista = cursor.fetchall()
+    conexion.close()
+    return lista
+
+def insert_partida(id_usuari, id_personatge, id_aventura):
+
+    conexion = conectar()
     cursor = conexion.cursor()
-    
-    print ("-----SELECCIÓN DE PERSONAJES-------")
+
+    query_insert = "INSERT INTO partida (id_usuari, id_personatge, id_aventura) VALUES (%s, %s, %s)"
+    try:
+        cursor.execute(query_insert, (id_usuari, id_personatge, id_aventura))
+        conexion.commit() 
+        print(f"Partida creada amb exit.")
+            
+            # CIERRA AQUÍ
+        cursor.close()
+        conexion.close()
+        return True
+        
+    except Exception as e:
+        # Y AQUÍ TAMBIÉN POR SI HAY ERROR
+        cursor.close()
+        conexion.close()
+        return False
+
+
+
+def cargar_personaje():
+    conexion = conectar()
+    cursor = conexion.cursor(dictionary=True)
+
+    print ("----Personajes DISPONIBLES----")
     cursor.execute("SELECT id_personatge, nom, descripcio FROM personatge")
     
+    lista = cursor.fetchall()
+    conexion.close()
+    return lista
     
 #-------------------------------------------------------------------------------------
--- M3_Programacio/game.py
+++ M3_Programacio/game.py
@@ -1,15 +1,52 @@
-#Fabian: Motor de juego, bucle incial y toma de desciciones 
-
-from funciones import *
+from funcions import *
 import time
+def verificador(tamaño, opcion):
+    
+    if isinstance(opcion, int):
+        if opcion <= tamaño:
+            return True
+        
+    return False    
+
+
+def game(user):
+    id_user = buscar_usuario(user)
+    ingame = True
+    while ingame:
+
+        aventuras = cargar_aventura()
+        mostrar_aventura(aventuras)
+        op_aventura = input("Elige aventura: ")
+        while (verificador(len(aventuras), op_aventura)):
+            print("Has introducido una opcion incorrecta")
+            op_aventura = input("Elige aventura: ")
+
+
+        personajes = cargar_personaje()
+        mostrar_personaje(personajes)
+        op_personaje = input("Elige personaje: ")
+        while (verificador(len(aventuras), op_personaje)):
+            print("Has introducido una opcion incorrecta")
+            op_personaje = input("Elige personaje: ")
+        
+        insert_partida(id_user[0], op_aventura, op_personaje)
+
+def mostrar_aventura(adventures):
+    for data in adventures:
+        print(data.get('id_aventura'), data.get('nom'), data.get('descripcio'))
+
+def mostrar_personaje(adventures):
+    for data in adventures:
+        print(data.get('id_personatge'), data.get('nom'), data.get('descripcio'))
 
 def main():
-    if not login():
+    user = login()
+    if not user:
         return
     while True:
         #menu principal
         print("\n" + "="*50)
-        print("          MENÚ PRINCIPAL")
+        print("MENÚ PRINCIPAL".center(50))
         print("="*50)
         print("1. Jugar Nueva Aventura")
         print("2. Salir")
@@ -21,92 +58,9 @@ def main():
             break
         
         elif opcion == "1":
-            #configuracion partida
+            game(user)
             
-            #seleccon de aventura
-            aventuras = get_adventures_with_chars()
-            if not aventuras:
-                print(">> Error: No se encontraron aventuras.")
-                continue
-                
-            print("\n--- AVENTURAS DISPONIBLES ---")
-            for id_adv, datos in aventuras.items():
-                print(f"ID [{id_adv}]: {datos['nom']}")
-                print(formatText(datos['descripcio'], 80))
-                print("-" * 20)
-                
-            try:
-                id_seleccion = int(input("\n>> ID Aventura: "))
-                if id_seleccion not in aventuras:
-                    print("ID no válido.")
-                    continue
-                game_context['idAdventure'] = id_seleccion
-            except ValueError:
-                print("Entrada inválida.")
-                continue
 
-            #selección de Personaje
-            personajes = get_characters()
-            print("\n--- SELECCIÓN DE PERSONAJE ---")
-            for i, p in enumerate(personajes):
-                print(f"{i+1}) {p['nom']} - {p['descripcio']}")
-                
-            try:
-                p_sel = int(input("\n>> Nº Personaje: "))
-                if 1 <= p_sel <= len(personajes):
-                    char_elegido = personajes[p_sel - 1]
-                    game_context['idCharacter'] = char_elegido['id_personatge']
-                    print(f"\n>> Personaje seleccionado: {char_elegido['nom']}")
-                else:
-                    print("Selección inválida.")
-                    continue
-            except ValueError:
-                continue
-
-            #inicializacion
-            insertCurrentGame()
-            print("\n>> Inicializando partida...")
-            time.sleep(1)
-            print("\n" + "#"*50)
-            print("      INICIO DE LA AVENTURA")
-            print("#"*50 + "\n")
-
-            #blucle game
-            
-            current_step_id = get_first_step_adventure(game_context['idAdventure'])
-            
-            if not current_step_id:
-                print("Error crítico: Aventura sin pasos definidos.")
-                continue
-
-            while True:
-                #recuperacion de datos del paso
-                step_data = get_step_data(current_step_id)
-                
-                #renderizado
-                print("\n" + formatText(step_data['descripcio_text'], 100))
-                
-                #condicion victoria/derrota
-                if step_data['es_final']:
-                    print("\n" + "="*30)
-                    print("       FIN DE LA PARTIDA")
-                    print("="*30)
-                    input("\n(Presiona Enter para continuar)")
-                    break
-                
-                #gestion opciones
-                opciones = get_answers_bystep_adventure(current_step_id)
-                
-                if not opciones:
-                    print("\n>> Error de lógica: Paso no final sin opciones.")
-                    break
-
-                #input usuario persistencia
-                eleccion = getOpt(opciones)
-                insertCurrentChoice(current_step_id, eleccion['id_opcio'])
-                
-                #avance estado
-                current_step_id = eleccion['id_pas_seguent']
 
 if __name__ == "__main__":
     main()
