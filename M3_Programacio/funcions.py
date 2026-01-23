5803fc75d059e35864ad4cfc00fe6deedb461a61
ErikCR1 <ecatalanrodriguez.25cf@iesesteveterradas.cat>
2026-01-23 02:53:20 +0100

    comentarios

--- M3_Programacio/funcions.py
+++ M3_Programacio/funcions.py
@@ -1,59 +1,67 @@
 import mysql.connector
 
-#--------------Conexion----------------
-def conectar():
+#--------------Conexion----------------  
+def conectar(): #Creamos una funcion para conectarse a la base de datos
     return mysql.connector.connect(
-        host="127.0.0.1",
-        port=3308,
-        user="super",
-        password="1234",
-        database="choose_your_story"
+        host="127.0.0.1", #El host 
+        port=3308, #El puerto del servidor
+        user="super", #El usuario
+        password="1234", # La contraseña
+        database="choose_your_story" #Y la base de datos que vayamos a usar
     )
 #--------------------------------------
 
-def buscar_usuario(query):
+def buscar_usuario(query): #Con esta funcion lo que queremos es sacar el id del usuario para luego poder printearlo por pantalla
     conexion = conectar()
     cursor = conexion.cursor()
 
-    a, b = str(query).split(";")
-    buscar = "SELECT id_usuari FROM usuari WHERE username = %s and password = %s"
+    a, b = str(query).split(";") #Como guardamos una variable string con el user y la contra con un ; vamos hacer que a sea el user y b la contra
+    #y con el split rompemos el;
+    buscar = "SELECT id_usuari FROM usuari WHERE username = %s and password = %s" #buscamos el id usuario de la tabla usuario donde user sea como el valor a y password como el valor b
     cursor.execute(buscar, (a,b))
-    resultado = cursor.fetchone()
+    resultado = cursor.fetchone() #recogemos solo una linea que coincida
     cursor.close()
     conexion.close()
-    return resultado
+    return resultado #y la devolvemos
 
 #-----------------Login----------------
-def login():
-    conexion = conectar()
+def login(): #Creamos una funcion para poder logearse
+    conexion = conectar() 
+    #Siempre antes de empezar algo donde necesitemos hacer algo con la bdd creamos una variable que sea conexion y sea igual a la funcion conectar
 
-    cursor = conexion.cursor()
+    cursor = conexion.cursor() #Creamos un cursor que nos servira para selecionar y executar cosas en especificos de la tabla
     print("-"*120)
     print("LOGIN / REGISTRO".center(120))
     print("-"*120)
     user = input("Introduce tu nombre de usuario: ")
     password = input("Introduce su contraseña: ")
 
-    buscar = "SELECT password FROM usuari WHERE username = %s"
-    cursor.execute(buscar, (user,))
-    resultado = cursor.fetchone()
+    buscar = "SELECT password FROM usuari WHERE username = %s" #Creamos una variable que es buscar y con comandos de sql selecionamos la contraseña
+    #de la tabla de usuarios donde username sea igual al valor dado que pondremos ahora
+
+    cursor.execute(buscar, (user,)) #ahora con el cursor de antes hacemos un execute para decir que con la variable de antes buscar haga el selct y el valor de %s es user
+    resultado = cursor.fetchone() # Hacemos una variable resultado donde el cursor apunta a solo la primera fila que coincide.
+    #Como en este caso buscas por username (que debería ser único), solo te interesa que te traiga esa fila específica.
+    # Intentamos pillar la contra del usuario que nos han dado.
     
-    string =  f"{user};{password}"
+    string =  f"{user};{password}" #Creamos un string con el usuario y la contraseña para la funcion de arriba
     
-    if resultado:
-        if resultado[0] == password:
+    if resultado: #Si resultado existe entonces: # Si hemos encontrado algo (significa que el usuario existe), entramos a chequear la contra
+        if resultado[0] == password: #vuelve a mirar si el resultado que es la contraseña es igual a la contra que nos han dado
             print(f"S'ha iniciat sessió correctament. Benvingut de nou, {user}!")
             input("Enter para continuar...")
-            return string
+            return string #si es que si nos devuelve el string que hemos echo antes
         else:
-            print("Error: La contrasenya és incorrecta per a aquest usuari.")
-            return False
-    else:
+            print("Error: La contrasenya és incorrecta per a aquest usuari.") #si la contraseña no es igual devuelve false
+            return False 
+    else: #Si resultado está vacío (significa que el usuario no existe en la bdd), nos ponemos a registrarlo como nuevo.
         print("L'usuari no existeix. Registrant nou usuari...")
-        query_insert = "INSERT INTO usuari (username, password) VALUES (%s, %s)"
-        try:
-            cursor.execute(query_insert, (user, password))
-            conexion.commit() 
+        query_insert = "INSERT INTO usuari (username, password) VALUES (%s, %s)" #creamos una variable de insert escribimos comando de sql donde dice que 
+        #en la tabla usuari especialmente y username y password le añadimos los valores dados que son %S que ahora se ponen 
+
+        try: #hacemos la prueba
+            cursor.execute(query_insert, (user, password)) #apuntamos a una ejecutacion donde la variable creada añade esos valores sustituyendo %s
+            conexion.commit() #se hace un commit
             print(f"Usuari '{user}' creat amb èxit.")
             input("Enter para continuar...")
             
@@ -62,45 +70,45 @@ def login():
             conexion.close()
             return string
         
-        except Exception as e:
+        except Exception as e: #Si no lo logra devuelve false
             # Y AQUÍ TAMBIÉN POR SI HAY ERROR
             cursor.close()
             conexion.close()
             return False
         
-def get_adventures_with_chars():
+def get_adventures_with_chars(): #Creamos una funcion para sacar las aventuras de la bdd
     conexion = conectar()
-    cursor = conexion.cursor(dictionary=True)
+    cursor = conexion.cursor(dictionary=True) #al añadir dictionary estamos haciendo que nos lo devuelva en forma de dicionario para luego trabajar con ellos
 
 
-    cursor.execute("SELECT id_aventura, nom, descripcio FROM aventura")
+    cursor.execute("SELECT id_aventura, nom, descripcio FROM aventura") 
     
-    lista = cursor.fetchall()
+    lista = cursor.fetchall() #hacemos una variable donde sacara TODAS las filas que encuentre en la tabla, distinto a fetchone que solo saca la primera fila (usada en el login)
     cursor.close()
     conexion.close()
-    return lista
+    return lista #cerramos para no gastar recursos y que no de errores y devolvemos la lista que tenemos de todos los diccionarios
 
-def insertCurrentGame(id_usuari, id_personatge, id_aventura):
+def insertCurrentGame(id_usuari, id_personatge, id_aventura): #Esta funcion la vamos a usar para insertar los id de las partidas
 
     conexion = conectar()
     cursor = conexion.cursor()
 
-    query_insert = "INSERT INTO partida (id_usuari, id_personatge, id_aventura) VALUES (%s, %s, %s)"
+    query_insert = "INSERT INTO partida (id_usuari, id_personatge, id_aventura) VALUES (%s, %s, %s)" #query insert  va ser un instert en la tabla partida de los id con los valores siguientes
     try:
-        cursor.execute(query_insert, (id_usuari, id_personatge, id_aventura))
-        conexion.commit() 
+        cursor.execute(query_insert, (id_usuari, id_personatge, id_aventura)) #los valores son las variables que pondremos en game.py
+        conexion.commit()  #un commit y un aviso de que hemos creado
         print(f"Partida creada amb exit.")
             
 
         cursor.close()
         conexion.close()
-        return True
+        return True #si sale bien devuelve un true
         
     except Exception as e:
         print(e)
         cursor.close()
         conexion.close()
-        return False
+        return False #si sale mal que imprima el error y un false
 
 #INSERT DE DECISIONES & EVENTOS
 def insertCurrentChoice(id_partida, id_pas, id_opcio_triada):
--- M3_Programacio/game.py
+++ M3_Programacio/game.py
@@ -8,21 +8,22 @@ def verificador(tamaño, opcion):
     return True    
 
 
-def game(user):
-    id_user = buscar_usuario(user)
-    ingame = True
+def game(user): #Esta es la funcion en la que funcionara todo el juego
+    id_user = buscar_usuario(user) #usamos la funcion de buscar usuario para encontrar el id del usuario que recordemos que user
+    #la funcion de login nos lo devolvio como user:password, ahora al usar esta funcion nos dara su ID
+    ingame = True #Empezamos con ingmae en True
 
-    while ingame:
-        aventuras = get_adventures_with_chars()
-        getFormatedAdventures(aventuras)
-        op_aventura = input("Elige aventura: ")
-        while (verificador(len(aventuras), op_aventura)):
+    while ingame: #Mientras ingame sea True
+        aventuras = get_adventures_with_chars() #la lista que nos devolvera la funcion pasara a ser aventuras
+        getFormatedAdventures(aventuras) #Y mostramos de forma bonita la lista con la funcion
+        op_aventura = input("Elige aventura: ") #Un input para elegir
+        while (verificador(len(aventuras), op_aventura)): #Usamos el verificador para que mientras la funcion sea true se repita 
             print("Has introducido una opcion incorrecta")
             getFormatedAdventures(aventuras)
             op_aventura = input("Elige aventura: ")
 
 
-        personajes = get_characters()
+        personajes = get_characters() #Esto es lo mismo que antes 
         getFormatedCharacters(personajes)
         op_personaje = input("Elige personaje: ")
         while (verificador(len(aventuras), op_personaje)):
@@ -30,30 +31,32 @@ def game(user):
             getFormatedCharacters(personajes)
             op_personaje = input("Elige personaje: ")
         
-        insertCurrentGame(id_user[0], op_aventura, op_personaje)
-        partida_actual = get_id_current_game()
+        insertCurrentGame(id_user[0], op_aventura, op_personaje) #Aqui usamos la funcion insert con el iduser que hemos sacado antes, la opcion aventura que ha elegido el usuario y la opcion
+        partida_actual = get_id_current_game() #partida actual va a pasar a ser la lista de los id de las partidas
 
 
-        evento = get_answers_bystep_adventure(op_aventura)
+        evento = get_answers_bystep_adventure(op_aventura) #ahora evento va a pasar a ser la lista de todos los dialogos con decisiones utilizando op aventura que a la hora de buscar en la funcion simula que es el id_aventura
         fin_partida = 0
         pas = 1
         id_pas = 1
         if int(op_aventura) == 2:
-                    id_pas = 20
-        while fin_partida == 0:
+                    id_pas = 20 #20 porque es el primer id que tiene la aventura 2
+        while fin_partida == 0: #Mientras fin de partida sea 0 se va a repetir
 
-            nombre_aventura = aventuras[int(op_aventura) - 1].get('nom')
+            nombre_aventura = aventuras[int(op_aventura) - 1].get('nom') #nombre de la aventura va a ser aventuras que recordemos que es la lista de las aventuras que hay
+            #con el indice del la opcion de aventura que elegio el usuario y menos uno porque las listas empiezan por 0. Con el .get devolvemos del diccionario el nom
             print("*"*120)
             print(str(nombre_aventura).center(120, "="))
             print("*"*120)
             print()
 
-            print(evento[pas - 1].get('descripcio_text'))
+            print(evento[pas - 1].get('descripcio_text')) #printeamos evento que es la lista de todos los dialogos con decisiones con un indice del pas que ahora es 1 por
+            #lo tanto es el primero pero restamos 1 porque las listas empiezan a contar por 0 
             print()
-            if int(evento[pas - 1].get('es_final')) == 1:
+            if int(evento[pas - 1].get('es_final')) == 1: #si el diccionario del evento con el pas 1  es = a 1 se acaba la partida 
                 fin_partida = 1
-            else:
-                decisiones = get_id_bystep_adventure(id_pas)
+            else: #si no
+                decisiones = get_id_bystep_adventure(id_pas) # decisiones pasa a ser la lista de los id de las decisiones que se pueden tomar con el id_pas que depende que aventura hayas escogido o al principio es 20 o 1
                 getFormatedAnswers(decisiones)
                 opcion_decision = input("Que decision escoges: ")
                 while (verificador(len(decisiones), opcion_decision)):
@@ -62,11 +65,15 @@ def game(user):
                     opcion_decision = input("Que decision escoges: ")
 
                 insertCurrentChoice(partida_actual[0].get ('id_partida'), evento[int(pas) - 1].get('id_pas'), decisiones[int(opcion_decision) - 1].get('id_opcio'))
-                pas = decisiones[int(opcion_decision) - 1].get('id_pas_seguent')
+                #una vez acabe el bucle significa que ha elegido opciones correctas, por lo que hacemos un insert con la funcion que hemos creado
+                #con partidaactual que es la lista de los id con un indice de 0 para sacar la primera partida de la lista, le sacamos el idpartida con get. evento que es la
+                #evento con indice pas - 1 y lo mismo con decisiones
+                pas = decisiones[int(opcion_decision) - 1].get('id_pas_seguent') # Actualizamos 'pas' con el ID del siguiente paso que viene de la decisión que ha tomado el usuario
                 id_pas = pas 
 
-                if int(op_aventura) == 2:
-                    pas -= 19
+                if int(op_aventura) == 2: 
+                    pas -= 19 # Si es la aventura 2, le restamos 19 
+                    #al ID para que el número coincida con la posición de nuestra lista, porque si usamos el 20 directamente nos salimos de la lista y da error.
                 
 
 
@@ -113,12 +120,12 @@ def getFormatedAnswers(decision):
         print()
         i += 1
 
-def main():
-    user = login()
-    while not user:
+def main(): #la funcion de main va a ser el menu principal
+    user = login() #llamamos la funcion login para que el usuario meta sus datos en la variable user
+    while not user: #mientras el user sea falso porque el login ha devuelto false entramos en un bucle hasta que lo haga bien 
         print("Contraseña incorrecta")
         user = login()
-    while True:
+    while True: #Una vez logeado creamos un bucle infito paara que siempre salga este menu 
         #menu principal
         print("\n" + "="*120)
         print("MENÚ PRINCIPAL".center(120))
@@ -129,13 +136,13 @@ def main():
         opcion = input("\n>> Opción: ")
         
         if opcion == "2":
-            print("Sesión finalizada.")
+            print("Sesión finalizada.") #si la opcion es 2 se hace un break y se acaba
             break
         
         elif opcion == "1":
-            game(user)
+            game(user) #si la opcion es 1 llamamos a la funcion game con la variable del user
             
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == "__main__": # Esto sirve para que el menú principal solo se abra si ejecutamos este archivo directamente. Si alguien importa este archivo desde otro sitio, el juego no se lanzará solo.
+    main() # Si estamos en el archivo principal, arrancamos el motor del juego.
